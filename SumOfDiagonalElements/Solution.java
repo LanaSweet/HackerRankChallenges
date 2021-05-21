681d2ce79268cd19832a8501a62bcf0f0a6ded7c
LanaSweet <gromivchuks@gmail.com>
2021-05-21 15:11:47 +0200

    Finds the absolute difference between the sum of the diagonal elements of a square array

---
+++ SumOfDiagonalElements/Solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+ 
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int sum1=0;
+         int sum2 = 0;
+        int n = arr.size();
+        for (int i=0;i<n; i++){
+            sum2 = sum2 + arr.get(i).get(n-i-1);
+            sum1 = sum1 + arr.get(i).get(i);
+        }
+       return Math.abs(sum1 - sum2);
+        
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < n; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
+            }
+
+            arr.add(arrRowItems);
+        }
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
