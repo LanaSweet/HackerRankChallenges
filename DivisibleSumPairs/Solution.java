0f13efd06eeac7ae0dc7407321107ff1cfca37ae
LanaSweet <gromivchuks@gmail.com>
2021-06-04 12:36:43 +0200

    Another HackerRank Challenge solved

---
+++ DivisibleSumPairs/Solution.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+ 
+    public static int divisibleSumPairs(int n, int k, List<Integer> ar) {
+        int pairCounter=0;
+        for(int i = 0; i<n; i++){
+            for (int j = i+1; j<n; j++){
+                if((ar.get(i)+ar.get(j))%k==0){
+                    pairCounter++;
+                }
+            }
+        }
+        return pairCounter;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        String[] arTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> ar = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arTemp[i]);
+            ar.add(arItem);
+        }
+
+        int result = Result.divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
